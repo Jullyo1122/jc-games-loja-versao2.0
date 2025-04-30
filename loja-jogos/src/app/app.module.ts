e456115bb4bf17f0ba6f88ac606f38ed7b072425
Jullyo1122 <jullyoc74@gmail.com>
2025-04-30 16:56:17 -0300

    Inclui diretorio

---
+++ loja-jogos/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
+import { CadastroProdutoComponent } from './componentes/cadastro-produto/cadastro-produto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    PainelPrincipalComponent,
+    CadastroProdutoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+import { FormsModule } from '@angular/forms'; 
+@NgModule({
+imports: [
+BrowserModule,
+AppRoutingModule,
+HttpClientModule,
+FormsModule 
+],
+})
+export class AppModule { }
